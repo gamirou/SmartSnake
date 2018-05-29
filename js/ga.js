eeac1957f4cde2820d031da6e5f085fa80ab9812
George Sima <bogdansima20@gmail.com>
2018-05-29 19:57:01 +0100

    Adding detailed comments to the file that has genetic algorithm

--- js/ga.js
+++ js/ga.js
@@ -1,23 +1,35 @@
 function nextGeneration() {
+    // All fitnesses get normalized
     calculateFitness();
     brains = [];
     bestSnakes = getBestSnakes();
 
     // First define all neural networks
     for (let i = 0; i < snakes.length-1; i++) {
+        // Pick two random parents
+        // NOTE: It can be the same parent for both of them
         parent1 = randchoice(bestSnakes);
         parent2 = randchoice(bestSnakes);
-        brains[i] = parent1.crossOver(parent2);
+
+        if (parent1 !== parent2) {
+            brains[i] = parent1.crossOver(parent2);
+        } else {
+            brains[i] = parent1;
+        }
     }
+    // Best one is added
     brains.push(bestSnakes[0]);
 
     // Then assign them to current snakes
     for (let i = 0; i < snakes.length; i++) {
         snakes[i].setBrain(brains[i]);
     }
-
 }
 
+/**
+ * Get the best brains based on their fitnesses
+ * @return {Array} Array of best "BEST_SNAKES" brains
+ */
 function getBestSnakes() {
     fitnesses = [];
     bestSnakes = [];
@@ -36,29 +48,13 @@ function getBestSnakes() {
         }
     }
 
-    for (let i =0 ;i < BEST_SNAKES; i++) {
+    for (let i = 0; i < BEST_SNAKES; i++) {
         bestSnakes.push(snakes[fitnesses[i][1]].brain)
     }
     return bestSnakes;
 
 }
 
-
-function pickOne() {
-    let index = 0;
-    let randomValue = Math.random();
-
-    while (randomValue > 0) {
-        randomValue = randomValue - snakes[index].fitness;
-        index++;
-    }
-    index--;
-
-    theChosenOne = snakes[index].brain;
-    theChosenOne.mutate(0.7);
-
-    return theChosenOne.copy();
-}
 
 function calculateFitness() {
     total = 0;
@@ -70,6 +66,13 @@ function calculateFitness() {
     }
 }
 
+
+/**
+ * It sorts an array based on an insertion sort
+ * @param  {Array} arr Array to be sorted
+ * @return {Array}     Sorted array
+ */
+/* Used only for debugging
 function insertionSort(arr) {
     sortedArr = [];
     oldArr = arr.slice();
@@ -83,3 +86,4 @@ function insertionSort(arr) {
 
     return sortedArr;
 }
+*/
