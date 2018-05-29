aaf0d183cbee120f83b49f85206534be31d0aa39
George Sima <bogdansima20@gmail.com>
2018-05-29 19:37:32 +0100

    Adding detailed comments to main.js

--- js/main.js
+++ js/main.js
@@ -24,6 +24,7 @@ let tiles = create2DArray(TILE_COLS, TILE_ROWS);
 let food;
 let snakes = [];
 
+// HTML reference to the debug slider
 let slider;
 
 function init() {
@@ -33,6 +34,7 @@ function init() {
     WIDTH = canvas.width;
     HEIGHT = canvas.height;
 
+    // Initializing tiles
     TILE_WIDTH = canvas.width / tiles.length;
 
     for (let i=0; i<TILE_ROWS; i++) {
@@ -41,10 +43,12 @@ function init() {
         }
     }
 
+    // Initializing snakes
     for (let i=0; i<TOTAL_SNAKES; i++) {
         snakes[i] = new Snake();
     }
 
+    // Pause event
     canvas.addEventListener("mousedown", function(event) {
         paused = !paused;
         pausedDraw = true;
@@ -72,7 +76,8 @@ function draw() {
         ctx.clearRect(0, 0, WIDTH, HEIGHT);
         ctx.closePath();
 
-        ctx.fillStyle='white';
+        // Background
+        ctx.fillStyle='black';
         ctx.fillRect(0, 0, WIDTH, HEIGHT);
 
         snakes[currentSnake].think();
@@ -81,8 +86,10 @@ function draw() {
 
         food.draw();
 
+        // If food is eaten
         if (snakes[currentSnake].ate()) {
             snakes[currentSnake].score += 100;
+            // Instead of making a new object, it only changes its position
             food.newSpot();
             let last_tail = snakes[currentSnake].tail[snakes[currentSnake].tail.length-1];
             snakes[currentSnake].tail.push(tiles[last_tail.row + snakes[currentSnake].dir.x][last_tail.col + snakes[currentSnake].dir.y]);
@@ -113,15 +120,28 @@ function draw() {
     frames++;
 }
 
+/**
+ * It updates debug stats
+ * @return {void}
+ */
 function updateHTML() {
     let paragraphs = document.getElementsByTagName("P");
     paragraphs[0].innerHTML = "Snake Number: " + currentSnake;
-    paragraphs[1].innerHTML = "Genertion Number: " + currentPopulation;
+    paragraphs[1].innerHTML = "Generation Number: " + currentPopulation;
     paragraphs[2].innerHTML = "Snake Fitness: " + snakes[currentSnake].score;
 }
 
+/************************************
+*             UTILITY               *
+************************************/
 
-// UTILITY
+
+/**
+ * Creates an empty 2D array
+ * @param  {Integer} lenI Number of columns
+ * @param  {Integer} lenJ Number of rows (optional if lenI != lenJ)
+ * @return {Array}       Returns the array
+ */
 function create2DArray(lenI, lenJ) {
     let x = new Array(lenI);
 
@@ -137,29 +157,46 @@ function create2DArray(lenI, lenJ) {
     return x;
 }
 
-
+/**
+ * Returns a random number
+ * @param  {Number} min Lower bound
+ * @param  {Number} max Upper bound
+ * @return {Number}     Usually, it returns a float
+ */
 function random(min, max) {
     return Math.random() * (max - min + 1) + min;
 }
 
-// Inclusive
+/**
+ * Returns a random integer
+ * @param  {Number} min Lower bound
+ * @param  {Number} max Upper bound
+ * @return {Number}     Returns an integer
+ */
 function randint(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-// Inclusive
+/**
+ * Returns a random item from array
+ * @param  {Array} arr         The array given
+ * @param  {Number} [offset=0] It lowers the range b yfirst and last "offset" items
+ * @return {any type}          Returns the random item
+ */
 function randchoice(arr, offset = 0) {
     return arr[randint(offset, arr.length - 1 - offset)];
 }
 
-/*
+/**
 Displays text with a given position, align and the size
-*** t - {String} - The text
-*** x, y - {Number} - X and y coordinates
-*** c - {String} - Colour of the text
-*** align - {String} - It aligns the text to center, left etc.
-*** size - {Number} - Size of the text
-*/
+ * @param  {String} t         Text given
+ * @param  {Number} x         X coordinate
+ * @param  {Number} y         Y coordinate
+ * @param  {String} c         Colour of the text
+ * @param  {String} align     Horizontal alignment
+ * @param  {Number} [size=25] Font size
+ * @return {void}
+ */
 function text(t, x, y, c, align, size = 25) {
     if (align) ctx.textAlign = align;
     ctx.font = "bold " +size+"px Arial";
@@ -167,14 +204,30 @@ function text(t, x, y, c, align, size = 25) {
     ctx.fillText(t,x,y);
 }
 
+/**
+ * Returns the distance between two points
+ * @param  {Number} x1 X coordinate of first point
+ * @param  {Number} y1 Y coordinate of first point
+ * @param  {Number} x2 X coordinate of second point
+ * @param  {Number} y2 Y coordinate of second point
+ * @return {Number}    Distance between two points
+ */
 function dist(x1, y1, x2, y2) {
     return (Math.sqrt((x1-x2) ** 2 + (y1-y2) ** 2))
 }
 
+/**
+ * Returns maximum number from array
+ * @return {Number} The maximum namber
+ */
 Array.prototype.max = function() {
     return Math.max.apply(null, this);
 };
 
+/**
+ * Returns minimum number from array
+ * @return {Number} The minimum namber
+ */
 Array.prototype.min = function() {
     return Math.min.apply(null, this);
 };
